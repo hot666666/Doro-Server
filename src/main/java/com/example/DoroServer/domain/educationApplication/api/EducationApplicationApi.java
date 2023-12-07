88002648dcc36109d8129d5e4e090eb49585d962
hot666666 <hotcs6@naver.com>
2023-12-07 20:33:52 +0900

    Feat: EducationApplication API에서 인증된 번호 사용 업데이트 #124 세션필터를 통해 phoneNumber를 얻어 EducationApplication API에서 사용합니다

--- src/main/java/com/example/DoroServer/domain/educationApplication/api/EducationApplicationApi.java
+++ src/main/java/com/example/DoroServer/domain/educationApplication/api/EducationApplicationApi.java
@@ -2,10 +2,12 @@ package com.example.DoroServer.domain.educationApplication.api;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -38,35 +40,46 @@ public class EducationApplicationApi {
     private final EducationApplicationService applicationService;
     private final ClassGroupService classGroupService;
 
+    @ModelAttribute("phoneNumber")
+    public String getPhoneNumber(HttpServletRequest request) {
+        // SessionFilter에서 넣어준 phoneNumber를 가져온다
+        return (String) request.getAttribute("phoneNumber");
+    }
+
     /* Education Application */
 
     @ApiOperation(value = "교육 신청 작성", notes = "교육을 신청합니다.")
     @PostMapping
-    public SuccessResponse<?> create(@RequestBody @Valid EducationApplicationReq applicationReq) {
-        EducationApplicationRes savedApplication = applicationService.save(applicationReq);
+    public SuccessResponse<?> create(@ModelAttribute("phoneNumber") String phoneNumber,
+            @RequestBody @Valid EducationApplicationReq applicationReq) {
+        EducationApplicationRes savedApplication = applicationService.save(applicationReq, phoneNumber);
         return SuccessResponse.successResponse(savedApplication);
     }
 
     @ApiOperation(value = "휴대폰 번호로 작성한 교육 신청 조회", notes = "신청한 교육을 핸드폰 번호를 통해 조회합니다.")
     @GetMapping
-    public SuccessResponse<?> read(@RequestBody @Valid RetrieveApplicationReq retrieveApplicationReq) {
+    public SuccessResponse<?> read(@ModelAttribute("phoneNumber") String phoneNumber,
+            @RequestBody @Valid RetrieveApplicationReq retrieveApplicationReq) {
         List<EducationApplicationRes> applications = applicationService
-                .findByPhoneNumber(retrieveApplicationReq);
+                .findByPhoneNumber(retrieveApplicationReq, phoneNumber);
         return SuccessResponse.successResponse(applications);
     }
 
     @ApiOperation(value = "신청한 교육 수정", notes = "신청한 교육을 수정합니다.")
     @PutMapping("/{applicationId}")
-    public SuccessResponse<?> update(@PathVariable Long applicationId,
+    public SuccessResponse<?> update(@ModelAttribute("phoneNumber") String phoneNumber,
+            @PathVariable Long applicationId,
             @RequestBody @Valid EducationApplicationReq applicationReq) {
-        EducationApplicationRes updatedApplication = applicationService.update(applicationId, applicationReq);
+        EducationApplicationRes updatedApplication = applicationService.update(applicationId, applicationReq,
+                phoneNumber);
         return SuccessResponse.successResponse(updatedApplication);
     }
 
     @ApiOperation(value = "신청한 교육 삭제", notes = "신청한 교육을 삭제합니다. 학급정보도 같이 삭제됩니다.")
     @DeleteMapping("/{applicationId}")
-    public SuccessResponse<?> delete(@PathVariable Long applicationId) {
-        applicationService.delete(applicationId);
+    public SuccessResponse<?> delete(@ModelAttribute("phoneNumber") String phoneNumber,
+            @PathVariable Long applicationId) {
+        applicationService.delete(applicationId, phoneNumber);
         return SuccessResponse.successResponse("신청이 삭제되었습니다.");
     }
 
@@ -74,25 +87,29 @@ public class EducationApplicationApi {
 
     @ApiOperation(value = "신청한 교육에 학급정보 추가", notes = "우선 교육을 신청해야 합니다.")
     @PostMapping("/{applicationId}/classGroup")
-    public SuccessResponse<?> addClassGroup(@PathVariable Long applicationId,
+    public SuccessResponse<?> addClassGroup(@ModelAttribute("phoneNumber") String phoneNumber,
+            @PathVariable Long applicationId,
             @RequestBody @Valid ClassGroupReq classGroupReq) {
-        ClassGroupRes savedClassGroup = classGroupService.addClassGroupToApplication(applicationId, classGroupReq);
+        ClassGroupRes savedClassGroup = classGroupService.addClassGroupToApplication(applicationId, classGroupReq,
+                phoneNumber);
         return SuccessResponse.successResponse(savedClassGroup);
     }
 
     @ApiOperation(value = "학급정보 업데이트", notes = "우선 교육을 신청해야 합니다.")
     @PutMapping("/{applicationId}/classGroup/{classGroupId}")
-    public SuccessResponse<?> updateClassGroup(@PathVariable Long applicationId, @PathVariable Long classGroupId,
+    public SuccessResponse<?> updateClassGroup(@ModelAttribute("phoneNumber") String phoneNumber,
+            @PathVariable Long applicationId, @PathVariable Long classGroupId,
             @RequestBody @Valid ClassGroupReq classGroupReq) {
         ClassGroupRes updatedClassGroup = classGroupService.updateClassGroup(applicationId, classGroupId,
-                classGroupReq);
+                classGroupReq, phoneNumber);
         return SuccessResponse.successResponse(updatedClassGroup);
     }
 
     @ApiOperation(value = "학급정보 삭제", notes = "학급정보가 삭제됩니다.")
     @DeleteMapping("/{applicationId}/classGroup/{classGroupId}")
-    public SuccessResponse<?> deleteClassGroup(@PathVariable Long applicationId, @PathVariable Long classGroupId) {
-        classGroupService.deleteClassGroup(applicationId, classGroupId);
+    public SuccessResponse<?> deleteClassGroup(@ModelAttribute("phoneNumber") String phoneNumber,
+            @PathVariable Long applicationId, @PathVariable Long classGroupId) {
+        classGroupService.deleteClassGroup(applicationId, classGroupId, phoneNumber);
         return SuccessResponse.successResponse("학급정보가 삭제되었습니다.");
     }
 }
