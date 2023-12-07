ba591f03cb2e2ecc423f5efac561efbc9da45670
hot666666 <hotcs6@naver.com>
2023-12-07 20:28:05 +0900

    Fix: DTO에서 휴대폰 번호 검증 추가 #124 - EducationApplicationReq - RetrieveApplicationReq

--- src/main/java/com/example/DoroServer/domain/educationApplication/dto/EducationApplicationReq.java
+++ src/main/java/com/example/DoroServer/domain/educationApplication/dto/EducationApplicationReq.java
@@ -3,6 +3,7 @@ package com.example.DoroServer.domain.educationApplication.dto;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -27,6 +28,7 @@ public class EducationApplicationReq {
     private String position; // 신청자 직위
 
     @NotBlank
+    @Pattern(regexp = "^01([016789])([0-9]{3,4})([0-9]{4})$", message = "올바른 휴대폰 번호 형식이 아닙니다.")
     private String phoneNumber; // 신청자 전화번호
 
     @Email
--- src/main/java/com/example/DoroServer/domain/educationApplication/dto/RetrieveApplicationReq.java
+++ src/main/java/com/example/DoroServer/domain/educationApplication/dto/RetrieveApplicationReq.java
@@ -1,6 +1,7 @@
 package com.example.DoroServer.domain.educationApplication.dto;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -15,5 +16,6 @@ import lombok.Builder;
 @AllArgsConstructor
 public class RetrieveApplicationReq {
     @NotBlank
+    @Pattern(regexp = "^01([016789])([0-9]{3,4})([0-9]{4})$", message = "올바른 휴대폰 번호 형식이 아닙니다.")
     private String phoneNumber; // 신청자 전화번호
 }
