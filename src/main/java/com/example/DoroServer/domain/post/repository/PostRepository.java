7197c680dc125120ee9cc4e31683f6ff970ed75e
hot666666 <hotcs6@naver.com>
2023-12-12 12:09:14 +0900

    Feat: 문의 관련 쿼리 추가 #120 - PostRepository

--- src/main/java/com/example/DoroServer/domain/post/repository/PostRepository.java
+++ src/main/java/com/example/DoroServer/domain/post/repository/PostRepository.java
@@ -2,7 +2,9 @@ package com.example.DoroServer.domain.post.repository;
 
 import com.example.DoroServer.domain.post.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
-public interface PostRepository extends JpaRepository<Post,Long>, PostRepositoryCustom {
-
+public interface PostRepository extends JpaRepository<Post, Long>, PostRepositoryCustom {
+    Page<Post> findByIsAnsweredFalse(Pageable pageable);
 }
